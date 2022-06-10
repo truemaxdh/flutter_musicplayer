0ee333bcba45e4ac76fa50b9534115d71a8ce533
truemaxdh <truemaxdh@gmail.com>
2022-06-10 17:16:05 +0900

    Update youtubePlayerWidget.dart

--- lib/youtubePlayerWidget.dart
+++ lib/youtubePlayerWidget.dart
@@ -2,24 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_music_player/main.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-Widget youtubePlayerWidget(BuildContext context, MyAppState _myAppState) {
+Widget youtubePlayerWidget() {
   YoutubePlayerController _controller = YoutubePlayerController(
-    initialVideoId: 'iLnmTe5Q2Qw',
-    flags: YoutubePlayerFlags(
-        autoPlay: true,
-        mute: true,
+    initialVideoId: 'K18cpp_-gP8',
+    params: YoutubePlayerParams(
+        playlist: ['nPt8bK2gbaU', 'gQDByCdjUXw'], // Defining custom playlist
+        startAt: Duration(seconds: 30),
+        showControls: true,
+        showFullscreenButton: true,
     ),
   );
-  return YoutubePlayer(
+  return YoutubePlayerIFrame(
     controller: _controller,
-    showVideoProgressIndicator: true,
-    videoProgressIndicatorColor: Colors.amber,
-    progressColors: ProgressColors(
-        playedColor: Colors.amber,
-        handleColor: Colors.amberAccent,
-    ),
-    onReady () {
-        _controller.addListener(listener);
-    },
+    aspectRatio: 16 / 9,
   );
 }
