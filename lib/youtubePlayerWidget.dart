8ceba247117b23aebd4bc2475523a366fc2b36b1
truemaxdh <truemaxdh@gmail.com>
2022-06-20 15:37:33 +0900

    Update youtubePlayerWidget.dart

--- lib/youtubePlayerWidget.dart
+++ lib/youtubePlayerWidget.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_music_player/main.dart';
 import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
+var _youtubeWidget = null;
 Widget youtubePlayerWidget() {
-  return Expanded(
-    child: YoutubePlayerIFrame(
-      controller: youtubePlayerController,
-      aspectRatio: 16 / 9,
-    ),
-  );
+  if (youtubeWidget == null) {
+    _youtubeWidget = Expanded(
+      child: YoutubePlayerIFrame(
+        controller: youtubePlayerController,
+        aspectRatio: 16 / 9,
+      ),
+    );
+  }
+  return _youtubeWidget;
 }
