60aa0781d1e22d804dee6421018096c09fe36dd5
truemaxdh <truemaxdh@gmail.com>
2022-06-21 14:51:27 +0900

    Update youtubePlayerWidget.dart

--- lib/youtubePlayerWidget.dart
+++ lib/youtubePlayerWidget.dart
@@ -2,18 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_music_player/main.dart';
 import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
-var _youtubeWidget = null;
 Widget youtubePlayerWidget() {
-  if (_youtubeWidget == null) {
-    title = "New Youtube Widget";
-    _youtubeWidget = Expanded(
-      child : YoutubePlayerControllerProvider( // Provides controller to all the widget below it.
-        controller: youtubePlayerController,
-        child: YoutubePlayerIFrame(
-          aspectRatio: 16 / 9,
-        ),
+  return Expanded(
+    child : YoutubePlayerControllerProvider( // Provides controller to all the widget below it.
+      controller: youtubePlayerController,
+      child: YoutubePlayerIFrame(
+        aspectRatio: 16 / 9,
       ),
-    );
-  }
-  return _youtubeWidget;
+    ),
+  );  
 }
