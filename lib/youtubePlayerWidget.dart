fbbea6aade564a10261045056f8c79f6a7b759ba
truemaxdh <truemaxdh@gmail.com>
2022-06-21 13:07:46 +0900

    Update youtubePlayerWidget.dart

--- lib/youtubePlayerWidget.dart
+++ lib/youtubePlayerWidget.dart
@@ -5,6 +5,7 @@ import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 var _youtubeWidget = null;
 Widget youtubePlayerWidget() {
   if (_youtubeWidget == null) {
+    title = "New Youtube Widget";
     _youtubeWidget = Expanded(
       child: YoutubePlayerIFrame(
         controller: youtubePlayerController,
