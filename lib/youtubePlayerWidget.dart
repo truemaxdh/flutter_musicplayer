0c29f8d91e15af193d5ccf9c7016763ab6fb75ba
truemaxdh <truemaxdh@gmail.com>
2022-06-21 13:45:20 +0900

    Update youtubePlayerWidget.dart

--- lib/youtubePlayerWidget.dart
+++ lib/youtubePlayerWidget.dart
@@ -7,7 +7,7 @@ Widget youtubePlayerWidget() {
   if (_youtubeWidget == null) {
     title = "New Youtube Widget";
     _youtubeWidget = Expanded(
-      YoutubePlayerControllerProvider( // Provides controller to all the widget below it.
+      child : YoutubePlayerControllerProvider( // Provides controller to all the widget below it.
         controller: youtubePlayerController,
         child: YoutubePlayerIFrame(
           aspectRatio: 16 / 9,
