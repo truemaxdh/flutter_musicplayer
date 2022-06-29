9cc9e7ffb8d79ca70be54774f9651bdbda44c668
truemaxdh <truemaxdh@gmail.com>
2022-06-29 16:13:43 +0900

    Update playerWidget.dart

--- lib/playerWidget.dart
+++ lib/playerWidget.dart
@@ -2,167 +2,175 @@ import 'package:flutter/material.dart';
 import 'package:flutter_music_player/main.dart';
 import 'package:flutter_music_player/youtubePlayerWidget.dart';
 
-Widget playerWidget(BuildContext context) {
-  return Container(
-    margin: EdgeInsets.all(5),
-    child: Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
-      Image(
-          height: 72,
-          width: 120,
-          fit: BoxFit.cover,
-          image: NetworkImage(
-              "https://avatars.githubusercontent.com/u/12081386?s=120&v=4")),
-      Expanded(
-        child: Container(
-          height: 72,
-          padding: EdgeInsets.symmetric(horizontal: 3),
-          child: Column(children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: songProgress(context),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 5),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  CircleAvatar(
-                    child: Center(
-                      child: IconButton(
+class PlayerWidget extends StatefulWidget {
+  @override
+  _PlayerWidget createState() => _PlayerWidget();
+}
+
+class _PlayerWidget extends State<PlayerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(5),
+      child: Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
+        Image(
+            height: 72,
+            width: 120,
+            fit: BoxFit.cover,
+            image: NetworkImage(
+                "https://avatars.githubusercontent.com/u/12081386?s=120&v=4")),
+        Expanded(
+          child: Container(
+            height: 72,
+            padding: EdgeInsets.symmetric(horizontal: 3),
+            child: Column(children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: songProgress(context),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    CircleAvatar(
+                      child: Center(
+                        child: IconButton(
+                            icon: Icon(
+                              Icons.skip_previous,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              playNextSong(-1);
+                            }),
+                      ),
+                      backgroundColor: Colors.cyan.withOpacity(0.3),
+                    ),
+                    CircleAvatar(
+                      radius: 23,
+                      child: Center(
+                        child: IconButton(
+                          onPressed: () async {
+                            if (songList[curSongIdx].isYoutube) {
+                              webviewController.callJsMethod(
+                                  (isPlaying
+                                      ? "pauseVideo"
+                                      : "playVideo"),
+                                  []);
+                            } else {
+                              isPlaying
+                                  ? audioPlayer.pause()
+                                  : audioPlayer.resume();
+                            }
+                          },
+                          padding: const EdgeInsets.all(0.0),
                           icon: Icon(
-                            Icons.skip_previous,
+                            isPlaying ? Icons.pause : Icons.play_arrow,
                             color: Colors.white,
                           ),
-                          onPressed: () {
-                            playNextSong(-1);
-                          }),
-                    ),
-                    backgroundColor: Colors.cyan.withOpacity(0.3),
-                  ),
-                  CircleAvatar(
-                    radius: 23,
-                    child: Center(
-                      child: IconButton(
-                        onPressed: () async {
-                          if (songList[curSongIdx].isYoutube) {
-                            webviewController.callJsMethod(
-                                (isPlaying
-                                    ? "pauseVideo"
-                                    : "playVideo"),
-                                []);
-                          } else {
-                            isPlaying
-                                ? audioPlayer.pause()
-                                : audioPlayer.resume();
-                          }
-                        },
-                        padding: const EdgeInsets.all(0.0),
-                        icon: Icon(
-                          isPlaying ? Icons.pause : Icons.play_arrow,
-                          color: Colors.white,
                         ),
                       ),
                     ),
-                  ),
-                  CircleAvatar(
-                    backgroundColor: Colors.cyan.withOpacity(0.3),
-                    child: Center(
-                      child: IconButton(
-                          icon: Icon(
-                            Icons.skip_next,
-                            color: Colors.white,
-                          ),
-                          onPressed: () {
-                            playNextSong(1);
-                          }),
+                    CircleAvatar(
+                      backgroundColor: Colors.cyan.withOpacity(0.3),
+                      child: Center(
+                        child: IconButton(
+                            icon: Icon(
+                              Icons.skip_next,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              playNextSong(1);
+                            }),
+                      ),
                     ),
-                  ),
-                  CircleAvatar(
-                    backgroundColor: Colors.cyan.withOpacity(0.3),
-                    child: Center(
-                      child: IconButton(
-                          icon: Icon(
-                            (screenMode == "player")
-                                ? Icons.expand_more
-                                : Icons.expand_less, //icons.expand_more,
-                            color: Colors.white,
-                          ),
-                          onPressed: () {
-                            myAppState.setState(() {
-                              screenMode =
-                                  (screenMode == "player") ? "mixed" : "player";
-                              if (songList[curSongIdx].isYoutube) {
-                                title = videoId;
-                              }
-                            });
-                          }),
+                    CircleAvatar(
+                      backgroundColor: Colors.cyan.withOpacity(0.3),
+                      child: Center(
+                        child: IconButton(
+                            icon: Icon(
+                              (screenMode == "player")
+                                  ? Icons.expand_more
+                                  : Icons.expand_less, //icons.expand_more,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              myAppState.setState(() {
+                                screenMode =
+                                    (screenMode == "player") ? "mixed" : "player";
+                                if (songList[curSongIdx].isYoutube) {
+                                  title = videoId;
+                                }
+                              });
+                            }),
+                      ),
                     ),
+                  ],
+                ),
+              ),
+            ]),
+          ),
+        ),
+      ]),
+    );
+  }
+  
+  String _formatDuration(Duration d) {
+    if (d == null) return "--:--";
+    int minute = d.inMinutes;
+    int second = d.inSeconds % 60;
+    String format = ((minute < 10) ? "0$minute" : "$minute") +
+        ":" +
+        ((second < 10) ? "0$second" : "$second");
+    return format;
+  }
+
+  Widget songProgress(BuildContext context) {
+    var style = TextStyle(color: Colors.black);
+    return Row(
+      children: <Widget>[
+        Text(
+          _formatDuration(Duration(seconds: sliderValue)),
+          style: style,
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            child: SliderTheme(
+                data: SliderTheme.of(context).copyWith(
+                  trackHeight: 2,
+                  thumbColor: Colors.blueAccent,
+                  overlayColor: Colors.blue,
+                  thumbShape: RoundSliderThumbShape(
+                    disabledThumbRadius: 5,
+                    enabledThumbRadius: 5,
                   ),
-                ],
-              ),
-            ),
-          ]),
-        ),
-      ),
-    ]),
-  );
-}
-
-String _formatDuration(Duration d) {
-  if (d == null) return "--:--";
-  int minute = d.inMinutes;
-  int second = d.inSeconds % 60;
-  String format = ((minute < 10) ? "0$minute" : "$minute") +
-      ":" +
-      ((second < 10) ? "0$second" : "$second");
-  return format;
-}
-
-Widget songProgress(BuildContext context) {
-  var style = TextStyle(color: Colors.black);
-  return Row(
-    children: <Widget>[
-      Text(
-        _formatDuration(Duration(seconds: sliderValue)),
-        style: style,
-      ),
-      Expanded(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 5),
-          child: SliderTheme(
-              data: SliderTheme.of(context).copyWith(
-                trackHeight: 2,
-                thumbColor: Colors.blueAccent,
-                overlayColor: Colors.blue,
-                thumbShape: RoundSliderThumbShape(
-                  disabledThumbRadius: 5,
-                  enabledThumbRadius: 5,
+                  overlayShape: RoundSliderOverlayShape(
+                    overlayRadius: 10,
+                  ),
+                  activeTrackColor: Colors.blueAccent,
+                  inactiveTrackColor: Colors.grey,
                 ),
-                overlayShape: RoundSliderOverlayShape(
-                  overlayRadius: 10,
-                ),
-                activeTrackColor: Colors.blueAccent,
-                inactiveTrackColor: Colors.grey,
-              ),
-              child: Slider(
-                min: 0,
-                max: duration.toDouble(),
-                value: sliderValue.toDouble(),
-                onChangeEnd: (value) {
-                  if (songList[curSongIdx].isYoutube) {
-                    //youtubePlayerController.seekTo(Duration(seconds: value.toInt()));
-                  } else {
-                    audioPlayer.seek(Duration(seconds: value.toInt()));
-                  }
-                },
-                onChanged: (double value) {},
-              )),
+                child: Slider(
+                  min: 0,
+                  max: duration.toDouble(),
+                  value: sliderValue.toDouble(),
+                  onChangeEnd: (value) {
+                    if (songList[curSongIdx].isYoutube) {
+                      //youtubePlayerController.seekTo(Duration(seconds: value.toInt()));
+                    } else {
+                      audioPlayer.seek(Duration(seconds: value.toInt()));
+                    }
+                  },
+                  onChanged: (double value) {},
+                )),
+          ),
         ),
-      ),
-      Text(
-        _formatDuration(Duration(seconds: duration)),
-        style: style,
-      ),
-    ],
-  );
+        Text(
+          _formatDuration(Duration(seconds: duration)),
+          style: style,
+        ),
+      ],
+    );
+  }
 }
