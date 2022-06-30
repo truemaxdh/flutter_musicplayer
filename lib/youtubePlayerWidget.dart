6024323caf087ec6696a2497e7b655d5d2f20b27
truemaxdh <truemaxdh@gmail.com>
2022-06-30 15:04:28 +0900

    Update youtubePlayerWidget.dart

--- lib/youtubePlayerWidget.dart
+++ lib/youtubePlayerWidget.dart
@@ -69,6 +69,9 @@ Widget youtubePlayerWidget(Size _size) {
       "      function playVideo() {" +
       "        player.playVideo();" +
       "      }" +
+      "      function seekTo(seconds, allowSeekAhead) {" +
+      "        player.seekTo(seconds, allowSeekAhead);" +
+      "      }" +
       "      function displayStatus() {" +
       "        var time = player.getCurrentTime();" +
       "        var duration = player.getDuration();" +
