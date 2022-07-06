b0dac40da56054c85130f6c6e1de3d5f7a5d3034
truemaxdh <truemaxdh@gmail.com>
2022-07-06 17:24:45 +0900

    Update hiveBase.dart

--- lib/hiveBase.dart
+++ lib/hiveBase.dart
@@ -14,20 +14,22 @@ final collection = await BoxCollection.open(
 // Open your boxes. Optional: Give it a type.
 final songList1 = collection.openBox<Map>('songList1');
 
-// Put something in
-await songList1.put(
-  '2002', 
-  {
-    'title': '2002', 
-    'artist': 'Anne Marie', 
-    'album_artwork': 'https://avatars.githubusercontent.com/u/12081386?s=120&v=4',
-    'mp3_url': '',
-    'ytb_video_id': 'Il-an3K9pjg'});
-await songList1.put(
-  'Space Trip', 
-  {
-    'title': 'Space Trip',
-    'artist': 'Danny Choi',
-    'album_artwork': 'https://avatars.githubusercontent.com/u/12081386?s=120&v=4',
-    'mp3_url': 'https://truemaxdh.github.io/MusicTreasureHouse/SpaceTrip/SpaceTrip.mp3',
-    'ytb_video_id': ''});
+function PutDBTestData() {
+  // Put something in
+  await songList1.put(
+    '2002', 
+    {
+      'title': '2002', 
+      'artist': 'Anne Marie', 
+      'album_artwork': 'https://avatars.githubusercontent.com/u/12081386?s=120&v=4',
+      'mp3_url': '',
+      'ytb_video_id': 'Il-an3K9pjg'});
+  await songList1.put(
+    'Space Trip', 
+    {
+      'title': 'Space Trip',
+      'artist': 'Danny Choi',
+      'album_artwork': 'https://avatars.githubusercontent.com/u/12081386?s=120&v=4',
+      'mp3_url': 'https://truemaxdh.github.io/MusicTreasureHouse/SpaceTrip/SpaceTrip.mp3',
+      'ytb_video_id': ''});
+}
