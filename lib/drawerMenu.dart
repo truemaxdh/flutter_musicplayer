0e523c58a89300da6991cb5f782ebeeedc20a61b
truemaxdh <truemaxdh@gmail.com>
2022-07-11 10:54:43 +0900

    Update drawerMenu.dart

--- lib/drawerMenu.dart
+++ lib/drawerMenu.dart
@@ -1,3 +1,34 @@
-Drawer getDrawer() {
-  return Drawer();
+Drawer getDrawer(context) {
+  return Drawer(
+    child: ListView(
+      // Important: Remove any padding from the ListView.
+      padding: EdgeInsets.zero,
+      children: [
+        const DrawerHeader(
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+          child: Text('Drawer Header'),
+        ),
+        ListTile(
+          title: const Text('Item 1'),
+          onTap: () {
+            // Update the state of the app
+            // ...
+            // Then close the drawer
+            Navigator.pop(context);
+          },
+        ),
+        ListTile(
+          title: const Text('Item 2'),
+          onTap: () {
+            // Update the state of the app
+            // ...
+            // Then close the drawer
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    ),
+  );
 };
