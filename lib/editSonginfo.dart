3e323729d0fbdc7bcfb91e5c7b5cde68c1dd5064
truemaxdh <truemaxdh@gmail.com>
2022-07-12 11:56:43 +0900

    Update editSonginfo.dart

--- lib/editSonginfo.dart
+++ lib/editSonginfo.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class EditSonginfoWidget extends StatelessWidget {
-  const EditSonginfoWidget({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
