1bec43b453527633982d7916a0c4ebdaf3bc9b35
truemaxdh <truemaxdh@gmail.com>
2022-07-12 17:27:13 +0900

    Update editSonginfo.dart

--- lib/editSonginfo.dart
+++ lib/editSonginfo.dart
@@ -7,14 +7,37 @@ class EditSonginfoWidget extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Add / Edit Song'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
-        ),
-      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter a search term',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Enter your username',
+              ),
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Go back!'),
+            ),
+          ),
+        ]
+      )
     );
   }
 }
