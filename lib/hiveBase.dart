237d44a216179425abe8402d7b85126e38aab3eb
maxdh <truemaxdh@gmail.com>
2022-07-16 23:54:36 +0900

    debug on web

--- lib/hiveBase.dart
+++ lib/hiveBase.dart
@@ -5,6 +5,7 @@ import 'package:flutter_music_player/main.dart';
 // Open your boxes. Optional: Give it a type.
 late CollectionBox<Map> box;
 Future<void> initSonglist() async {
+  print('point1');
   // Create a box collection
   final collection = await BoxCollection.open(
     'MySongs', // Name of your database
@@ -46,9 +47,12 @@ Future<void> initSonglist() async {
       32
     ]), // Key to encrypt your boxes (Only used in Flutter / Dart IO)
   );
-
+  print('point2');
   box = await collection.openBox<Map>('SongList1');
-  box.getAllValues().then((values) async {
+  print('point3:$box');
+  await box.getAllValues().then((values) async {
+    print('point4');
+
     if (values.isEmpty) {
       // Put something in
       await box.put('2002', {
@@ -72,7 +76,9 @@ Future<void> initSonglist() async {
         'mp3Url': '',
         'ytbVideoId': 'iT267zwmFBw'
       });
+      print('point5:$box');
     }
+    print('point6');
   });
 
   songList = await box.getAllValues();
